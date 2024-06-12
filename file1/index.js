a7fd806c47814054fc6948db083080f3a5bf7dd9
kaushikvishwakarma <vkaushik13804@gmail.com>
2024-06-12 18:59:43 +0530

    funciton testing

--- file1/index.js
+++ file1/index.js
@@ -1,5 +1,4 @@
-function Mainfunction(){
-    return(<h1>oh yeah monster on the lose </h1>)
-}
-ReactDOM.render(<div><Mainfunction /></div>,document.getElementById("root"))
-``
+const h1=document.createElement("h1")
+h1.textContent = "This is an imperative way to program"
+h1.className="header"
+document.getElementById("root").append(h1)
