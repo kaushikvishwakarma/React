49d692cf891a4de2c70db0f29bf652a98fd24efc
kaushikvishwakarma <vkaushik13804@gmail.com>
2024-06-12 19:29:33 +0530

    funciton testing

--- file1/index.js
+++ file1/index.js
@@ -1,4 +1,23 @@
-const h1=document.createElement("h1")
-h1.textContent = "This is an imperative way to program"
-h1.className="header"
-document.getElementById("root").append(h1)
+// // const h1=document.createElement("h1")
+// // h1.textContent = "This is an imperative way to program"
+// // h1.className="header"
+// // document.getElementById("root").append(h1)
+// const page=(<div>
+//     <h1 className="header">Hello bro</h1>
+//     <p>This is a paragraph</p>
+// </div>)
+// console.log(page)
+// ReactDOM.render(page,document.getElementById("root"))
+
+
+const navbar=(
+    <nav>
+        <h1>Kaushik's Restro</h1>
+        <ul>
+            <li>Menu</li>
+            <li>About</li>
+            <li>Contact</li>
+            </ul> 
+    </nav>
+)
+ReactDOM.render(navbar,document.getElementById("root"));
