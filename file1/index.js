fbea23ea2ecee7628d540593b68d0e2dc43541e3
kaushikvishwakarma <vkaushik13804@gmail.com>
2024-06-13 19:10:00 +0530

    function

--- file1/index.js
+++ file1/index.js
@@ -10,14 +10,50 @@
 // ReactDOM.render(page,document.getElementById("root"))
 
 
-const navbar=(
-    <nav>
-        <h1>Kaushik's Restro</h1>
-        <ul>
-            <li>Menu</li>
-            <li>About</li>
-            <li>Contact</li>
-            </ul> 
-    </nav>
-)
-ReactDOM.render(navbar,document.getElementById("root"));
+// const navbar=(
+//     <nav>
+//         <h1>Kaushik's Restro</h1>
+//         <ul>
+//             <li>Menu</li>
+//             <li>About</li>
+//             <li>Contact</li>
+//             </ul> 
+//     </nav>
+// )
+// ReactDOM.render(navbar,document.getElementById("root"));
+// const page=(<div>
+//     <h1>My awesome website in react</h1>
+//     <h3>Reasons I love </h3>
+//     <ol>
+//         <li>bla bla</li>
+//     </ol>
+// </div>)
+// function Tempy(){
+//     return(
+//     <div>
+//         <h1>Kaushik's Restro</h1>
+//         <ul>
+//             <li>Menu</li>
+//             <li>About</li>
+//             <li>Contact</li>
+//             </ul> 
+//     </div>
+//     )
+// }
+
+
+// ReactDOM.render(<Tempy />,document.getElementById("root"))
+
+
+
+function Lolopopo(){
+return (<div>
+    <ol>
+        <li>BLA BLA BLA</li>
+        <li>PHEW PHEW</li>
+        <li>LANDROVER LELE MERA BC</li>
+    </ol>
+</div>
+)    
+}
+ReactDOM.render(<Lolopopo />,document.getElementById("root"))
